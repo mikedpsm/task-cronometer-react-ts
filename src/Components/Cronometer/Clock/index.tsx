3c893960801f2f8f5fda6d96c715368f1a98f337
Maicon Douglas <maicondpsm@gmail.com>
2023-07-17 23:32:58 -0300

    Adds several implementarions such as finishing a task

--- src/Components/Cronometer/Clock/index.tsx
+++ src/Components/Cronometer/Clock/index.tsx
@@ -1,13 +1,21 @@
 import style from './Clock.module.scss';
 
-export default function Clock() {
+interface Props {
+  time: number | undefined
+}
+
+export default function Clock({ time = 0 }: Props) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const [minuteTens, minuteUnit] = String(minutes).padStart(2, "0");
+  const [secondTens, secondUnit] = String(seconds).padStart(2, "0");
   return (
     <>
-      <span className={style.clockNumber} >0</span>
-      <span className={style.clockNumber}>0</span>
+      <span className={style.clockNumber} >{minuteTens}</span>
+      <span className={style.clockNumber}>{minuteUnit}</span>
       <span className={style.clockDivision}>:</span>
-      <span className={style.clockNumber}>0</span>
-      <span className={style.clockNumber}>0</span>
+      <span className={style.clockNumber}>{secondTens}</span>
+      <span className={style.clockNumber}>{secondUnit}</span>
     </>
   )
 }
--- src/Components/Cronometer/index.tsx
+++ src/Components/Cronometer/index.tsx
@@ -2,15 +2,42 @@ import Button from '../Button';
 import Clock from './Clock';
 import style from './Cronometer.module.scss';
 import { timeToSeconds } from '../../common/utils/time';
+import { ITask } from '../../types/task';
+import { useEffect, useState } from 'react';
 
-export default function Cronometer() {
+interface Props {
+  selected: ITask | undefined,
+  finishTask: () => void
+}
+
+export default function Cronometer({ selected, finishTask }: Props) {
+  const [time, setTime] = useState<number>();
+
+  useEffect(() => {
+    if (selected?.time) {
+      setTime(timeToSeconds(selected.time));
+    }
+  }, [selected]);
+
+  function countBackwards(counter: number = 0) {
+    setTimeout(() => {
+      if (counter > 0) {
+        setTime(counter - 1);
+        return countBackwards(counter - 1);
+      }
+      finishTask();
+    }, 1000);
+  }
+
   return (
     <div className={style.cronometer}>
       <p className={style.title}>Choose a card and start the cronometer</p>
       <div className={style.clockWrapper}>
-        <Clock />
+        <Clock time={time} />
       </div>
-      <Button>Start</Button>
-    </div>
+      <Button onClick={() => countBackwards(time)}>
+        Start
+      </Button>
+    </div >
   )
 }
